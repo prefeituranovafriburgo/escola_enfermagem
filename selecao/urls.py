66e2a18834e23c2d5a1227b3b9e29d2fa364fa57
Loyola Desktop <loyola@jlb.net.br>
2022-11-07 13:03:17 -0300

    att

--- selecao/urls.py
+++ selecao/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # path('inicio_teste', views.inicio_teste, name='inicio_teste'),
     #
     # path('', views.inicio, name='inicio'),
-    path('cadastro/46320<id>82', views.cadastro, name='cadastro'),
+    #path('cadastro/46320<id>82', views.cadastro, name='cadastro'),
     path('imprime/<str:chave>', views.imprime, name='imprime'),
     path('consulta', views.consulta, name='consulta'),
     path('consulta_chave/<str:chave>', views.consulta_chave, name='consulta_chave'),
